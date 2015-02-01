fb8890f884ebed5781b01e36f32c8efe07e75eb4
Agustiadi <leeagustiadi@gmail.com>
2015-02-01 18:08:06 +0800

    added Login and SignUp capabilities

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -9,15 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var signupActive = false
+    
+    // Setting up IB Outlet connections for labels and buttons
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var loginBtnLbl: UIButton!
+    @IBOutlet weak var signupToggleBtnLbl: UIButton!
+    
+    // Function that runs when the Sign-Up Here Toggle Button is pressed
+    @IBAction func signupToggleBtnPressed(sender: AnyObject) {
+        
+        if signupActive == false {
+            emailInput.alpha = 1
+            loginBtnLbl.setTitle("Sign Up", forState: UIControlState.Normal)
+            signupToggleBtnLbl.setTitle("Log in here !", forState: UIControlState.Normal)
+            signupActive = true
+            
+        } else {
+            emailInput.alpha = 0
+            loginBtnLbl.setTitle("Log In", forState: UIControlState.Normal)
+            signupToggleBtnLbl.setTitle("Sign up here !", forState: UIControlState.Normal)
+            signupActive = false
+        }
+    }
+    
+    // Function that runs when the Login/Sign up button is pressed
+    @IBAction func loginSignupBtn(sender: AnyObject) {
+        if signupActive == true {
+            
+            if usernameInput.text == "" || emailInput.text == "" || passwordInput.text == ""{
+                displayAlert("Missing Input", message: "You need to give input for all three fields")
+            } else if countElements(passwordInput.text) < 8 {
+                displayAlert("Password Length", message: "Please note that your password needs to consist of at least 8 charcaters")
+            } else {
+                var user = PFUser()
+                user.username = usernameInput.text
+                user.email = emailInput.text
+                user.password = passwordInput.text
+                user.signUpInBackgroundWithBlock {
+                    (succeeded: Bool!, signupError: NSError!) -> Void in
+                    if signupError == nil {
+                        println("Successfully signed up")
+                        self.jumpToUserTable()
+                    } else {
+                        if let errorString = signupError.userInfo?["error"] as? NSString{
+                            self.displayAlert("Error in Form", message: errorString)
+                            
+                        } else {
+                            self.displayAlert("Error", message: "Some unidentified error. Please try again!")
+                        }
+                        
+                    }
+                }
+            }
+            
+        } else {
+            
+            PFUser.logInWithUsernameInBackground(usernameInput.text, password:passwordInput.text) {
+                (user: PFUser!, error: NSError!) -> Void in
+                if user != nil {
+                    println("Login as \(user.username) is successful")
+                    self.jumpToUserTable()
+                    
+                } else {
+                    self.displayAlert("Log In Error", message: "Please make sure you input the right username and password")
+                }
+            }
+            
+        }
+
+    }
+    
+    // Function that calls the various UIAlert
+    func displayAlert(title: String, message: String){
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func jumpToUserTable() {
+        
+        if PFUser.currentUser() != nil {
+            performSegueWithIdentifier("login", sender: self)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        println("testing")
+        // Initial set-up for Login page
+        emailInput.alpha = 0
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
